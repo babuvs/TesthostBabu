fe89377f3c401a04837f36dd08134541eb47b677
babuvs <gmbabuji@gmail.com>
2023-09-14 16:00:01 +0530

    checkuns fixed

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,13 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { WelcomeModule } from '../app/pages/welcome/welcome.module';
 
 const routes: Routes = [
   { path: '', pathMatch: 'full', redirectTo: '/welcome' },
-  { path: 'welcome', loadChildren: () => import('./pages/welcome/welcome.module').then(m => m.WelcomeModule) }
+  { path: 'welcome', loadChildren: () => import('../app/pages/welcome/welcome.module').then(m => m.WelcomeModule) }
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes), WelcomeModule],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
